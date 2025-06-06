46113824c6f5cb6f798e7a0a7d451e344a022ce6
krishinamurti <questionyouk@gmail.com>
2025-06-06 23:57:28 +0900

    error correction

--- rag_pdf_modify.py
+++ rag_pdf_modify.py
@@ -7,7 +7,7 @@ from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain.vectorstores import Chroma
+from langchain_community.vectorstores import Chroma
 from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
