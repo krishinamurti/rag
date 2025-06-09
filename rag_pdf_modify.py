6df80f06de22020f4a5b24c207d7f443697e86b7
krishinamurti <questionyouk@gmail.com>
2025-06-09 23:10:57 +0900

    final db path change

--- rag_pdf_modify.py
+++ rag_pdf_modify.py
@@ -276,7 +276,7 @@ else:
                           #      st.markdown(doc.metadata['source'], help=doc.page_content)
         except:
             st.header("📚 PDF 업로드 해주세요!")
-            st.header("한번에 여러 파일 업로드")
+            #st.header("한번에 여러 파일 업로드")
 
     elif selection == "Database":
         #cache_clear()
